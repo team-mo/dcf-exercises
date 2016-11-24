2d8fd0a5cb8bca5aeba855334ccdd5aba77d87a9
Luke Tanprasit <tanprasitp@hotmail.co.uk>
2016-11-24 17:41:28 +0000

    Revealing image metadata will now be binded to onclick events

--- ex1/app/js/renderer.js
+++ ex1/app/js/renderer.js
@@ -14,7 +14,7 @@ $('.front').on(
     }
 )
 
-$(".front").on('drop', function (e) {
+$('.front').on('drop', function (e) {
       e.preventDefault()
       // get the img file from the drop event
       var file = e.originalEvent.dataTransfer.files[0];
@@ -22,14 +22,25 @@ $(".front").on('drop', function (e) {
        // Only process image files.
       if (file.type.match('image.*')) {
             // display the uploaded img
-            $('#img-box').attr("src", file.path);
+            $('#img-box').attr('src', file.path);
       } else {
             // display a broken img in the future
+            $('#img-box').attr('src', './img/not-img.png');
       }
 
       // display the img meta data
-      $("#fileName").text("Name: " + file.name);
-      $("#fileModified").text("Last Modified: " + file.lastModifiedDate.toLocaleDateString());
-      $("#fileType").text("Type: " + file.type);
-      $("#fileSize").text("Size: " + Math.round(file.size / Math.pow(1024,2)) + "MB");
+      $('#fileName').text('Name: ' + file.name);
+      $('#fileModified').text('Last Modified: ' + file.lastModifiedDate.toLocaleDateString());
+      $('#fileType').text('Type: ' + file.type);
+      $('#fileSize').text('Size: ' + Math.round(file.size / Math.pow(1024,2)) + 'MB');
+})
+
+/*Add an on click event for image*/
+$('.flip-container').on('click', function (e) {
+  e.preventDefault()
+  if ($(this).hasClass('hover')) { 
+      $(this).removeClass('hover')
+  } else {
+      $(this).addClass('hover')
+  }
 })
