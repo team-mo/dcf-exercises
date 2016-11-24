3302277b60af26c889cee965be01454d011afdde
Luke Tanprasit <tanprasitp@hotmail.co.uk>
2016-11-24 16:39:51 +0000

    Completed bare bones version of the meta data app

---
+++ ex1/app/js/renderer.js
@@ -0,0 +1,35 @@
+$('.front').on(
+    'dragover',
+    function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+    }
+)
+
+$('.front').on(
+    'dragenter',
+    function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+    }
+)
+
+$(".front").on('drop', function (e) {
+      e.preventDefault()
+      // get the img file from the drop event
+      var file = e.originalEvent.dataTransfer.files[0];
+
+       // Only process image files.
+      if (file.type.match('image.*')) {
+            // display the uploaded img
+            $('#img-box').attr("src", file.path);
+      } else {
+            // display a broken img
+      }
+
+      // display the img meta data
+      $("#fileName").text("Name: " + file.name);
+      $("#fileModified").text("Last Modified: " + file.lastModifiedDate.toLocaleDateString());
+      $("#fileType").text("Type: " + file.type);
+      $("#fileSize").text("Size: " + Math.round(file.size / Math.pow(1024,2)) + "MB");
+})
--- ex1/main.js
+++ ex1/main.js
@@ -1,6 +1,7 @@
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
+require('electron-reload')( __dirname )
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -8,7 +9,11 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+   win = new BrowserWindow(
+    {
+      width: 800, 
+      height: 600,
+    })
 
   // and load the index.html of the app.
   win.loadURL(url.format({
